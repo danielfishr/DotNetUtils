360aac1fdcc34c63491ec110305bf45db45439d2
Dan Fisher <danielfishr@gmail.com>
2012-10-12 11:02:58 +0100

    Added docs

--- DotNetUtils/RandomUtil.cs
+++ DotNetUtils/RandomUtil.cs
@@ -4,11 +4,17 @@ namespace DotNetUtils
 {
     public static class RandomUtil
     {
-        private static Random random = new Random();
-
-        public static int Next(int inclusiveMinValue, int exclusiveMaxValue)
+        private static readonly Random _random = new Random();
+        /// <summary>
+        /// A more sane API prevents common mistake of the same seed causing seemingly none random numbers.
+        /// Prevents Random _random = new Random(); being duplicated everywhere
+        /// </summary>
+        /// <param name="inclusiveMinVal"></param>
+        /// <param name="exclusiveMaxVal"></param>
+        /// <returns></returns>
+        public static int Next(int inclusiveMinVal, int exclusiveMaxVal)
         {
-            return random.Next(inclusiveMinValue,exclusiveMaxValue)
+            return _random.Next(inclusiveMinVal, exclusiveMaxVal);
         }
     }
 }
