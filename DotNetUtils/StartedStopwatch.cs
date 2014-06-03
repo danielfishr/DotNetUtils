adf0474f2733d2a2d5d35f33efef04d670673442
danielfishr <danielfishr@gmail.com>
2014-06-03 11:49:49 +0100

    started stop watch to save them LoC

---
+++ DotNetUtils/StartedStopwatch.cs
@@ -0,0 +1,17 @@
+﻿namespace DotNetUtils
+{
+    using System.Diagnostics;
+
+    public static class StartedStopwatch
+    {
+        public static Stopwatch New
+        {
+            get
+            {
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+                return stopwatch;
+            }
+        }
+    }
+}
