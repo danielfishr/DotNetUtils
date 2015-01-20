a3892db78ef69d1fdcecd1184f604b7b378b9bee
danielfishr <danielfishr@gmail.com>
2015-01-20 13:21:54 +0000

    null reference check

--- DotNetUtils/PhoneNumbeUtil.cs
+++ DotNetUtils/PhoneNumbeUtil.cs
@@ -4,6 +4,10 @@ namespace DotNetUtils
     {
         public static bool LooksLikeMobileNumber(string phoneNumber)
         {
+            if (string.IsNullOrWhiteSpace(phoneNumber))
+            {
+                return false;
+            }
             string digits = StringUtil.RemoveNoneDigits(phoneNumber).Replace(" ", string.Empty);
             return (digits.StartsWith("07") && digits.Length == 11) || (digits.StartsWith("447") && digits.Length == 12);
         }
